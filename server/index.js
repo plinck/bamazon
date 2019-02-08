e7e0fd14f406c63b1d31b8c26f3d57db42e9e777
plinck <paul.linck@gmail.com>
2019-02-08 18:26:57 -0500

    Made render generic to display any table

--- server/index.js
+++ server/index.js
@@ -7,50 +7,47 @@ const BAmazonModel = require("./BAmazonModel.js");
 // Render Products to the console
 // To make the header and data line up, I find maximum val for each column and make them all that size
 function renderProducts(rows) {
-    let headers = ["product_id", "product_name", "department_name", "price", "stock_quantity", "product_sales"];
-    const columnWidths = [];
+    const columnWidths = {};
 
-    for (let i in headers) {
-        let h = headers[i];
-        columnWidths[i] = h.length;
+    // first, get header (keys) length and set as default
+    // note keys in array of keys so the name is keys[idx]
+    let keys = (rows['0'] != undefined) ? Object.keys(rows['0']) : {};
+    for (let i in keys) {
+        columnWidths[keys[i]] = keys[i].length;
     }
 
-    // Increase header width if value is wider
+    // Calculate column width based on max row size for each column
     rows.forEach(row => {
-        let i = 0;
         for (let key in row) {
             let str = row[key].toString();
-            columnWidths[i] = Math.max(columnWidths[i], str.length);
-            i++;
+            columnWidths[key] = Math.max(columnWidths[key], str.length);
         }
     });
 
     // display the header
     let headerText = "|";
-    for (let i in headers) {
-        headerText += " " + headers[i] + " ".repeat(columnWidths[i] - headers[i].length) + " |";
+    for (let key in columnWidths) {
+        headerText += " " + key + " ".repeat(columnWidths[key] - key.length) + " |";
     }
     console.log();
     console.log(headerText.toUpperCase());
 
     headerText = "|";
-    for (let i in headers) {
-        headerText += " " + "-".repeat(headers[i].length) + "-".repeat(columnWidths[i] - headers[i].length) + " |";
+    for (let key in columnWidths) {
+        headerText += " " + "-".repeat(key.length) + "-".repeat(columnWidths[key] - key.length) + " |";
     }
     console.log(headerText.toUpperCase());
 
-    for (let i in rows) {
-        //console.log(row);
-        let row = rows[i];
+    // Now, print the data
+    rows.forEach(row => {
         let rowText = "|";
-        let j = 0;
         for (let key in row) {
             let str = row[key].toString();
-            rowText += " " + row[key] + " ".repeat(columnWidths[j] - str.length) + " |";
-            j++;
+            rowText += " " + row[key] + " ".repeat(columnWidths[key] - str.length) + " |";
         }
         console.log(rowText);
-    }
+
+    });
 }
 
 // Render Departments to the console
